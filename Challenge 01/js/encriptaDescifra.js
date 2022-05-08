bcb786a46317eee9482cdeb7a2553ef34b5cdf47
quetzalx <quetzalx@gmail.com>
2022-05-08 16:44:39 -0500

    Terminé el descifrado pero sin optimizar, fata hacer el boton copiar

--- Challenge 01/js/encriptaDescifra.js
+++ Challenge 01/js/encriptaDescifra.js
@@ -5,55 +5,42 @@ function encriptaTexto(){
    var textoAcifrar=document.querySelector("#textoPlano").value;
    const vocales=['e', 'i', 'a', 'o', 'u'];
    const llave =['nter', 'mes', 'i', 'ber', 'fat']
-   /*const tAC=textoAcifrar.split('');*/
    const textoEnArreglo = textoAcifrar.split('');
 
    for (var i = 0; i < textoEnArreglo.length; i++) {
-      for (var j = 0; j <= vocales.length; j++) {
+      for (var j = 0; j < vocales.length; j++) {
          if (textoEnArreglo[i]==vocales[j]) {
             textoEnArreglo[i]=textoEnArreglo[i]+llave[j];
          }
       }
-      console.log(textoEnArreglo);
+      /*console.log(textoEnArreglo);*/
    }
-
-/*   for (var i = 0; i < textoEnArreglo.length; i++) {
-      if (textoEnArreglo[i]=="e") {
-         textoEnArreglo[i]=textoEnArreglo[i]+"nter";
-      }
-      if (textoEnArreglo[i]=="i") {
-         textoEnArreglo[i]=textoEnArreglo[i]+"mes"
-      }
-      if (textoEnArreglo[i]=="a") {
-         textoEnArreglo[i]=textoEnArreglo[i]+"i"
-      }
-      if (textoEnArreglo[i]=="o") {
-         textoEnArreglo[i]=textoEnArreglo[i]+"ber"
-      }
-      if (textoEnArreglo[i]=="u") {
-         textoEnArreglo[i]=textoEnArreglo[i]+"fat"
-      }
-      console.log(textoEnArreglo[i]);
-    }*/
-    let texto=textoEnArreglo.join('');
-    textoACaja.value=texto;
-
-/*    var indice=0;
-    tAC.forEach(function(elemento, indice){
-       if (elemento[indice]==vocales[indice]) {
-          elemento[indice]=elemento[indice]+llave[indice];
-          console.log("Compara elementos y vocales");
-          console.log(elemento[indice]);
-       }
-    });
-    console.log(tAC);*/
+   let texto=textoEnArreglo.join('');
+   textoACaja.value=texto;
 }
 
 function descifraTexto(){
-   console.log("Cuando click en descifrado")
-}
+    var textoADescifrar=document.querySelector("#textoPlano").value;
+    const vocales=['e', 'i', 'a', 'o', 'u'];
+    const llave =['enter', 'imes', 'ai', 'ober', 'ufat']
+    const textoEnArreglo = textoADescifrar.split('');
+    const texto=textoADescifrar;
 
+/*    for (var i = 0; i < 4; i++) {
+        textoF=texto.replace(/llave[i]/g,vocales[i]);
+        console.log(texto);
+    }
+*/
+    textof=textoADescifrar.replace(/enter/g,'e');
+    console.log(textof);
+    textofuno=textof.replace(/imes/g,'i');
+    console.log(textofuno);
+    textofdos=textofuno.replace(/ai/g,'a');
+    console.log(textofdos);
+    textoftres=textofdos.replace(/ober/g,'o');
+    console.log(textoftres);
+    textofcuatro=textoftres.replace(/ufat/g,'u');
 
-function resultado(){
-
+    console.log(textofcuatro);
+    textoACaja.value=textofcuatro;
 }
