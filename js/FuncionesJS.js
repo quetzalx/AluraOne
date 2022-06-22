8cee210f5f7b0a736a350e3266806e8084c6afaf
quetzalx <quetzalx@gmail.com>
2022-06-22 12:19:09 -0500

    Modifiqué el programa 20 y terminé el index, sólo falta agregar los demás programas

---
+++ js/FuncionesJS.js
@@ -0,0 +1,7 @@
+var clickejercicio=document.querySelector(".botonesEj");
+let listacodigos=["programa1.html", "programa2.html", "programa3.html"]
+
+function nuevaPagina(Ejercicio) {
+    /*var indice=Ejercicio;*/
+    window.open("01. Lógica de Programación/programa"+Ejercicio+".html");
+}
